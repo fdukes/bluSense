e38f4a6c4fe51f89f0f83f6cfa3a195f7522c671
Forrest Dukes <fdukes@utexas.edu>
2013-04-15 14:02:53 -0500

    First Commit of App

---
+++ 7thSense/src/com/seventh/sense/Bluetooth.java
@@ -0,0 +1,15 @@
+package com.seventh.sense;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+
+public class Bluetooth {
+	public Bluetooth () {
+	}
+	
+	public void setUpBT (){
+		
+	}
+	
+	
+}
